d3028eafdeb10c134b836b7cf66b3166654c9502
Sagar3992 <sagar.wadhwa@hotmail.com>
2020-09-25 09:31:11 -0700

    Text preprocessed to remove punctuation/alpahnum/digits/stopwords, lemmatize, equate words with common stem, and build a list of all words in processed texts

--- Scripts/cloudGen.py
+++ Scripts/cloudGen.py
@@ -26,3 +26,35 @@ translator = Translator()
 lang = detect(text)
 if lang != 'en':
     text = translator.translate(text)
+ 
+def alterText(text, stemmer, stopWords, lemmatizer):
+    alteredWord = []
+    stem_words = dict()
+    #remove any additional spacing etc
+    words = re.findall(r'\w+', text, flags = re.UNICODE)
+    for word in words:
+        word = word.lower()
+        #remove alphanum and digits
+        word = ''.join([i for i in word if not i.isdigit() and i.isalnum()])
+        #remove stopWords
+        if(word not in stopWords):
+            #lemamtize all the words
+            word = lemmatizer.lemmatize(word)
+            #remove stem words, while preserving one of the words for each stem, because we need to create word cloud for the de-stemmed word and not just the stem
+            temp_word = word
+            new_word = stemmer.stem(word)
+            if new_word in stem_words.keys():
+                stem_words[new_word].append(temp_word)
+            else:
+                stem_words[new_word] = [temp_word]
+            word = stem_words[new_word][0]
+            alteredWord.append(word)
+        else:
+            continue
+    return alteredWord, stem_words
+
+stemmer = SnowballStemmer("english")
+stopWords = nltk.corpus.stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+text, stem_words = alterText(text, stemmer, stopWords, lemmatizer)
+print(text)
