7518605e80ae3c04547e0e9b74fdfe8fa88e1187
Sagar3992 <sagar.wadhwa@hotmail.com>
2020-09-25 09:43:11 -0700

    translate text to eng, if text not eng

--- Scripts/cloudGen.py
+++ Scripts/cloudGen.py
@@ -2,15 +2,27 @@ import activateEnv
 activateEnv.activate()
 import argparse
 import databaseAccessor
+import nltk
+from langdetect import detect
+from googletrans import Translator
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--DocId', help='Mention the DocumentID of Firebase stored text', required=True)
+parser.add_argument('-d', '--DocumentId', help='Mention the DocumentID of Firebase stored text', required=True)
 args = parser.parse_args() 
-documentId = args.DocId
+documentId = args.DocumentId
 
 dbData = databaseAccessor.read_document(documentId)
 if not (dbData):
     print("No such document")
     exit()
 
-print(dbData)
+text = dbData['Text']
+
+#translate text if not english
+translator = Translator()
+lang = detect(text)
+if lang != 'en':
+    text = translator.translate(text)
